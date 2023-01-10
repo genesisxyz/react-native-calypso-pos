3b5d62cda346bec386b36abe3384218d6c3280e7
Matteo Pastorino <matteoomega99@gmail.com>
2023-01-10 17:10:38 +0100

    feat: add ByteUtils

---
+++ android/src/main/java/com/pos/ByteUtilsModule.kt
@@ -0,0 +1,36 @@
+package com.pos
+
+import com.facebook.react.bridge.*
+import com.pos.byte_stuff.ByteConvertReactNativeUtil
+import com.pos.byte_stuff.ByteConvertStringUtil
+import java.nio.charset.StandardCharsets
+
+class ByteUtilsModule (reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext){
+
+  override fun getName(): String {
+    return ByteUtilsModule.NAME
+  }
+
+  @ReactMethod
+  fun stringToByteArray(str: String, promise: Promise){
+    val bytes = ByteConvertStringUtil.stringToByteArray(str)
+    promise.resolve(ByteConvertReactNativeUtil.byteArrayToReadableArray(bytes))
+  }
+
+  @ReactMethod
+  fun bytesFromString(str: String, promise: Promise){
+    val res = str.toByteArray(StandardCharsets.UTF_8)
+    promise.resolve(ByteConvertReactNativeUtil.byteArrayToReadableArray(res))
+  }
+
+  @ReactMethod
+  fun bytesToHexString(array: ReadableArray, promise: Promise){
+    val bytes = ByteConvertReactNativeUtil.readableArrayToByteArray(array)
+    val hexString = ByteConvertStringUtil.bytesToHexString(bytes)
+    promise.resolve(hexString)
+  }
+
+  companion object {
+    const val NAME = "ByteUtils"
+  }
+}
---
+++ src/ByteUtils.ts
@@ -0,0 +1,30 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-pos' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const ByteUtils = NativeModules.ByteUtils
+  ? NativeModules.ByteUtils
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export async function bytesToHexString(array: number[]): Promise<string> {
+  return await ByteUtils.bytesToHexString(array);
+}
+
+export async function bytesFromString(str: string): Promise<number[]> {
+  return await ByteUtils.bytesFromString(str);
+}
+
+export async function stringToByteArray(str: string): Promise<number[]> {
+  return await ByteUtils.stringToByteArray(str);
+}
